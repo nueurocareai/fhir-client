1f0db393fd5be42051037a4b6b58f8bfaa155dcf
azharali046 <azharali046@gmail.com>
2024-02-27 10:39:49 +0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -32,8 +32,10 @@
           var lname = '';
 
           if (typeof patient.name[0] !== 'undefined') {
+            console.log( patient.name[0].family);
             fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+            lname = patient.name[0].family;
+            //lname = patient.name[0].family.join(' ');
           }
 
           var height = byCodes('8302-2');
