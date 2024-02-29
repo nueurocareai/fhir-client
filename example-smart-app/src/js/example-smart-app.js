85ea15d8a0087ecb63c59e44ca6689621d99637f
azharali046 <azharali046@gmail.com>
2024-02-29 12:26:47 +0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -52,6 +52,7 @@
           var ldl = byCodes('2089-1');
 
           var p = defaultPatient();
+          var pApp = patientApp();
           p.birthdate = patient.birthDate;
           p.gender = gender;
           p.fname = fname;
@@ -72,6 +73,7 @@
           // Process appointment data
           if (appt && appt.length > 0) {
             // Here you can process the appointment data received from the API
+            console.log('Appointments:', appt.data.entry[0]);
             var appointmentData = appt.data.entry[0];
             var id= appointmentData.resource.id;
             var status = appointmentData.resource.status;
@@ -80,13 +82,13 @@
             var endDate = appointmentData.resource.end;
             var actor = appointmentData.resource.participant[0].actor.display;
             var patient_name = patient.name[0].given.join(' ');
-            p.id = id;
-            p.status = status;
-            p.description = description;
-            p.start_date = startDate;
-            p.end_date = endDate;
-            p.actor = actor;
-            p.patient = patient_name;
+            pApp.id = id;
+            pApp.status = status;
+            pApp.description = description;
+            pApp.start_date = startDate;
+            pApp.end_date = endDate;
+            pApp.actor = actor;
+            pApp.patient = patient_name;
             // For example, you can extract specific appointment details and include them in the patient object
             // p.appointments = appt.map(appointment => ({
             //   // Extract and format relevant appointment details
@@ -116,13 +118,18 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+
+    };
+  }
+    function patientApp(){
+    return {
       id: {value: ''},
       status: {value: ''},
       description: {value: ''},
-      start_date: {value:''},
+      start_date: {value: ''},
       end_date: {value: ''},
-      actor:{value: ''},
-       patient:{value: ''},
+      actor: {value: ''},
+      patient: {value: ''},
     };
   }
 
@@ -166,13 +173,13 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
-    $('#id').html(p.id);
-    $('#status').html(p.status);
-    $('#description').html(p.description);
-    $('#start_date').html(p.start_date);
-    $('#end_date').html(p.end_date);
-    $('#actor').html(p.actor);
-    $('#patient').html(p.patient);
+    $('#id').html(pApp.id);
+    $('#status').html(pApp.status);
+    $('#description').html(pApp.description);
+    $('#start_date').html(pApp.start_date);
+    $('#end_date').html(pApp.end_date);
+    $('#actor').html(pApp.actor);
+    $('#patient').html(pApp.patient);
   };
 
 })(window);
